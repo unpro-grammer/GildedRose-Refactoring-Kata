f991385fbb5f295ea795510385f69817fc8de8c5
kblincoe <kblincoe@acm.org>
2021-08-16 14:32:25 +1200

    adding initial code and tests

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# GildedRose-Refactoring-Kata
-Starting code for the GildedRose Refactoring Kata for SOFTENG 283
+# Gilded Rose Refactoring Kata
+
+This Kata was originally created by Terry Hughes (http://twitter.com/TerryHughes). It was then translated into many programming languages (including the Java version used here) by Emily Bache (https://github.com/emilybache/GildedRose-Refactoring-Kata).  
+
+I have also included a set of tests written by Sandra Parsick (https://github.com/sparsick) which can be used to ensure that nothing is broken while doing the refactoring exercise. 
+
+The ["Gilded Rose Requirements"](https://github.com/kblincoe/GildedRose-Refactoring-Kata/blob/main/GildedRoseRequirements.txt) explains what the code is for. These requirements are also included as comments in the ["GildedRoseTest.java"](https://github.com/kblincoe/GildedRose-Refactoring-Kata/blob/main/Java/src/test/java/com/gildedrose/GildedRoseTest.java) class.
+
+## How to use this Kata
+
+1. Clone this repository so that you can add a "new feature" to the code. You can create a new Java project with the cloned code in your favorite IDE. Be sure to add the JUnit5 library to your project so that you can run the provided test suite.
+
+2. Modify the code to add a new feature. Your task is to enable selling of "conjured" items, which degrade in Quality twice as fast as normal items. There are two failing test cases, which should pass once the code is successfully modified to add this new feature. While you do this code modification, take note of the following: How readable is the code? How easy is the code to modify? How flexible is the code? i.e., could you easily add another item?
+
+3. Consider the principles of clean code and code smells. What code smells are present in the code? What clean code principles are violated? 
+
+4. Refactor the code to address the issues you have identified. Be careful not to break any of the existing functionality (check all tests will still pass). 
+ 
+_Constraints for all code modifications / refactoring:_ Feel free to make any changes to the UpdateQuality method and add any new code as long as everything still works correctly. However, **do not alter** the Item class or Items property.
---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,260 @@
+//Tests from: 
+//https://github.com/sparsick/coding-katas/blob/master/GildedRose-Refactoring-Kata-Java/com/gildedrose/GildedRoseTest.java
+//NOTE: tests are only for the purpose of validating you do not break the code when adding new features / refactoring
+//      Proper test construction will be covered later in the course
+
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GildedRoseTest {
+
+	//All items have a name
+    @Test
+    public void checkItemName() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("foo", app.items[0].name);
+    }
+    
+    //All items have a SellIn value which denotes the number of days we have to sell the item
+    //All items have a Quality value which denotes how valuable the item is
+	//At the end of each day our system lowers both values for every item
+    @Test
+    public void decreaseSellDate() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(-1, app.items[0].sellIn);
+    }
+    
+    @Test
+    public void decreaseQuality() {
+        Item[] items = new Item[] { new Item("foo", 0, 1) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
+    }
+
+    //Once the sell by date has passed, Quality degrades twice as fast
+    @Test
+    public void decreaseQualityTwice() {
+        Item[] items = new Item[] { new Item("foo", -1, 2) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
+    }
+    
+    @Test
+    public void decreaseQualityTwiceWithNoNegativeValue() {
+        Item[] items = new Item[] { new Item("foo", -1, 1) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
+    }
+
+    //The Quality of an item is never negative
+    @Test
+    public void qualityCannotBeNegative() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
+    }
+
+    //"Aged Brie" actually increases in Quality the older it gets
+    @Test
+    public void increaseQualityForAgedBrieItem() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 1) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(2, app.items[0].quality);
+    }
+    
+    @Test
+    public void increaseQualityTwicedForAgedBrieItemBecauseSellInIsPassed() {
+        Item[] items = new Item[] { new Item("Aged Brie", -2, 1) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(3, app.items[0].quality);
+    }
+
+    @Test
+    public void increaseQualityTwicedForAgedBrieItemBecauseSellInIs0() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 1) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(3, app.items[0].quality);
+    }
+
+    //The Quality of an item is never more than 50
+    @Test
+    public void qualityCannotBeGreaterThan50() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 50) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    public void qualityIncreasedTwiceCannotBeGreaterThan50() {
+        Item[] items = new Item[] { new Item("Aged Brie", -2, 50) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(50, app.items[0].quality);
+    }
+
+    //"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+    //Just for clarification, an item can never have its Quality increase above 50, however "Sulfuras" is a
+    //legendary item and as such its Quality is 80 and it never alters.
+    @Test
+    public void sulfurasItemNeverBeSold() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 2, 80) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(2, app.items[0].sellIn);
+    }
+
+    @Test
+    public void sulfurasItemNeverDecreaseQuality() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 2, 80) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(80, app.items[0].quality);
+    }
+
+    //"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+	//Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+	//Quality drops to 0 after the concert
+    @Test
+    public void checkQualityForBackstagePasses11Daysleft() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 11, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(11, app.items[0].quality);
+    }
+
+    @Test
+    public void checkQualityForBackstagePasses10Daysleft() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(12, app.items[0].quality);
+    }
+
+    @Test
+    public void checkQualityCannotBeGreaterThan50ForBackstagePasses10Daysleft() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    public void checkQualityForBackstagePasses5Daysleft() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(13, app.items[0].quality);
+    }
+
+    @Test
+    public void checkQualityCannotBeGreaterThan50ForBackstagePasses5Daysleft() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 50) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    public void checkQualityForBackstagePasses0Daysleft() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    public void checkQualityForBackstagePassesAfterConcert() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", -1, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
+    }
+
+    //"Conjured" items degrade in Quality twice as fast as normal items
+    //TODO: code should be updated to get below tests to pass
+    @Test
+    public void checkQualityForConjuredItemsBeforeSellInPassed(){
+        Item[] items = new Item[] { new Item("Conjured", 2, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(8, app.items[0].quality);
+    }
+
+    @Test
+    public void checkQualityForConjuredItemsAfterSellInPassed(){
+        Item[] items = new Item[] { new Item("Conjured", -2, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(6, app.items[0].quality);
+    }
+
+    @Test
+    public void checkSellInForConjuredItems(){
+        Item[] items = new Item[] { new Item("Conjured", 2, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(1, app.items[0].sellIn);
+    }
+
+}
